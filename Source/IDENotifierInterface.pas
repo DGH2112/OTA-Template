648597700bb81782ad414b0de4daff6f03ff94b9
DGH2112 <davidghoyle@gmail.com>
2012-03-28 20:53:04 +0100

    Initial import of Chapter 15

--- Source/IDENotifierInterface.pas
+++ Source/IDENotifierInterface.pas
@@ -1,3 +1,12 @@
+(**
+
+  This module contains a class that handles the IDENotifier interface.
+
+  @Version 1.0
+  @Author  David Hoyle
+  @Date    24 Mar 2012
+
+**)
 Unit IDENotifierInterface;
 
 Interface
@@ -8,6 +17,7 @@ Uses
 {$INCLUDE CompilerDefinitions.inc}
 
 Type
+  (** A class to handle the IDENotifier interface. **)
   TIDENotifierTemplate = Class(TNotifierObject,
     IOTANotifier,
     {$IFDEF D0005} IOTAIDENotifier50, {$ENDIF}
@@ -40,52 +50,134 @@ Implementation
 
 Uses
   SysUtils,
-  UtilityFunctions;
+  UtilityFunctions,
+  ApplicationOptions;
 
 Const
+  (** A constant array represent true and false as strings. **)
   strBoolean : Array[False..True] Of String = ('False', 'True');
 
 {$IFDEF D0006}
 ResourceString
+  (** A resource string for the message group under which messages should be
+      output. **)
   strIDENotifierMessages = 'IDE Notifier Messages';
 {$ENDIF}
 
 { TIDENotifierTemplate }
 
 {$IFDEF D0005}
+(**
+
+  This method is called by the Delphi IDEs up to 4, before a project is
+  compiled.
+
+  @precon  Project is a reference to the project about to be compiled and Cancel
+           should be changed to true IF you which to stop the IDE from
+           continuing to compile.
+  @postcon Place handling in the method for things you want to do before the
+           project is compiled.
+
+  @param   Project as an IOTAProject as a constant
+  @param   Cancel  as a Boolean as a reference
+
+**)
 Procedure TIDENotifierTemplate.BeforeCompile(Const Project: IOTAProject;
   Var Cancel: Boolean);
 
 Begin
-  OutputMessage(Format('BeforeCompile: Project: %s, Cancel = %s', [
-    ExtractFileName(Project.FileName), strBoolean[Cancel]])
-    {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage(Format('BeforeCompile: Project: %s, Cancel = %s', [
+      ExtractFileName(Project.FileName), strBoolean[Cancel]])
+      {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
+(**
+
+  This method is called by the Delphi IDEs 5 and above before a project is
+  compiled.
+
+  @precon  Project is a reference to the project about to be compiled,
+           IsCodeInsight is true for compilation instigated by the code insight
+           IDE functionality and Cancel should be changed to true IF you
+           which to stop the IDE from continuing to compile.
+  @postcon Place handling in the method for things you want to do before the
+           project is compiled.
+
+  @param   Project       as an IOTAProject as a constant
+  @param   IsCodeInsight as a Boolean
+  @param   Cancel        as a Boolean as a reference
+
+**)
 Procedure TIDENotifierTemplate.BeforeCompile(Const Project: IOTAProject;
   IsCodeInsight: Boolean; Var Cancel: Boolean);
 
 Begin
-  OutputMessage(Format('BeforeCompile: Project: %s, IsCodeInsight = %s, Cancel = %s', [
-    ExtractFileName(Project.FileName), strBoolean[IsCodeInsight], strBoolean[Cancel]])
-    {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage(Format('BeforeCompile: Project: %s, IsCodeInsight = %s, Cancel = %s', [
+      ExtractFileName(Project.FileName), strBoolean[IsCodeInsight], strBoolean[Cancel]])
+      {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
+(**
+
+  This method is called by the Delphi IDEs up to 4, after a project is
+  compiled.
+
+  @precon  Succeeded is true of the project was successfully compiled.
+  @postcon Place handling in the method for things you want to do after the
+           project is compiled.
+
+  @param   Succeeded as a Boolean
+
+**)
 Procedure TIDENotifierTemplate.AfterCompile(Succeeded: Boolean);
 
 Begin
-  OutputMessage(Format('AfterCompile: Succeeded=  %s', [strBoolean[Succeeded]])
-    {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage(Format('AfterCompile: Succeeded=  %s', [strBoolean[Succeeded]])
+      {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
+(**
+
+  This method is called by the Delphi IDEs 5, 6, 7 and 8, after a project is
+  compiled.
+
+  @precon  Succeeded is true of the project was successfully compiled and
+           IsCodeInsight is true if the compilation is invoked by the IDEs
+           CodeInsight functionality.
+  @postcon Place handling in the method for things you want to do after the
+           project is compiled.
+
+  @param   Succeeded     as a Boolean
+  @param   IsCodeInsight as a Boolean
+
+**)
 Procedure TIDENotifierTemplate.AfterCompile(Succeeded, IsCodeInsight: Boolean);
 
 Begin
-  OutputMessage(Format('AfterCompile: Succeeded=  %s, IsCodeInsight = %s', [
-    strBoolean[Succeeded], strBoolean[IsCodeInsight]])
-    {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage(Format('AfterCompile: Succeeded=  %s, IsCodeInsight = %s', [
+      strBoolean[Succeeded], strBoolean[IsCodeInsight]])
+      {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
+(**
+
+  This method is fired when files are opened, closed along with projects,
+  desktops and packages.
+
+  @precon  NotifyCode is the type of File Notification, Filename is the name of
+           the file and Cancel can be set to true to cancel the operation.
+  @postcon Place code here to perform additional tasks when files are opened
+           and closed.
+
+  @param   NotifyCode as a TOTAFileNotification
+  @param   FileName   as a String as a constant
+  @param   Cancel     as a Boolean as a reference
+
+**)
 Procedure TIDENotifierTemplate.FileNotification(NotifyCode: TOTAFileNotification;
   Const FileName: String; Var Cancel: Boolean);
 
@@ -104,50 +196,108 @@ Const
   );
 
 Begin
-  OutputMessage(Format('FileNotification: NotifyCode = %s, FileName = %s, Cancel = %s', [
-    strNotifyCode[NotifyCode], ExtractFileName(FileName), strBoolean[Cancel]])
-    {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage(Format('FileNotification: NotifyCode = %s, FileName = %s, Cancel = %s', [
+      strNotifyCode[NotifyCode], ExtractFileName(FileName), strBoolean[Cancel]])
+      {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
 {$ENDIF}
 
 {$IFDEF D2005}
+(**
+
+  This method is called by the Delphi IDEs 2005 and above, after a project is
+  compiled.
+
+  @precon  Succeeded is true of the project was successfully compiled, Project
+           is a reference to the project being compiled and IsCodeInsight is
+           true if the compilation is invoked by the IDEs CodeInsight
+           functionality.
+  @postcon Place handling in the method for things you want to do after the
+           project is compiled.
+
+  @param   Project       as an IOTAProject as a constant
+  @param   Succeeded     as a Boolean
+  @param   IsCodeInsight as a Boolean
+
+**)
 Procedure TIDENotifierTemplate.AfterCompile(Const Project: IOTAProject;
   Succeeded, IsCodeInsight: Boolean);
 
 Begin
-  OutputMessage(Format('AfterCompile: Project: %s, Succeeded=  %s, IsCodeInsight = %s', [
-    ExtractFileName(Project.FileName), strBoolean[Succeeded], strBoolean[IsCodeInsight]]),
-    strIDENotifierMessages);
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage(Format('AfterCompile: Project: %s, Succeeded=  %s, IsCodeInsight = %s', [
+      ExtractFileName(Project.FileName), strBoolean[Succeeded], strBoolean[IsCodeInsight]]),
+      strIDENotifierMessages);
 End;
 {$ENDIF}
 
+(**
+
+  This method is not fired for a IDE notifier.
+
+  @precon  None.
+  @postcon None.
+
+**)
 Procedure TIDENotifierTemplate.AfterSave;
 
 Begin
-  OutputMessage('AfterSave' {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage('AfterSave' {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
 
+(**
+
+  This method is not fired for a IDE notifier.
+
+  @precon  None.
+  @postcon None.
+
+**)
 Procedure TIDENotifierTemplate.BeforeSave;
 
 Begin
-  OutputMessage('BeforeSave' {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage('BeforeSave' {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
+(**
+
+  This method is not fired for a IDE notifier.
+
+  @precon  None.
+  @postcon None.
+
+**)
 Procedure TIDENotifierTemplate.Destroyed;
 
 Begin
-  OutputMessage('Destroyed' {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage('Destroyed' {$IFDEF D0006}, strIDENotifierMessages {$ENDIF});
 End;
 
+(**
+
+  This method is not fired for a IDE notifier.
+
+  @precon  None.
+  @postcon None.
+
+**)
 Procedure TIDENotifierTemplate.Modified;
 
 Begin
-  OutputMessage('Modified' {$IFDEF D0006} , strIDENotifierMessages {$ENDIF});
+  If moShowIDEMessages In ApplicationOps.ModuleOps Then
+    OutputMessage('Modified' {$IFDEF D0006} , strIDENotifierMessages {$ENDIF});
 End;
 
+(** A null intialisation section **)
 Initialization
+(** Ensure that any messages are removed from the IDE before unloading the
+    package. **)
 Finalization
   ClearMessages([cmCompiler..cmTool]);
 End.
